f818ba46e7e5bc8dd3e08aa2601e967024f85e1f
InetaKr <ineta.kriunaite@outlook.com>
2022-12-30 16:05:34 +0200

    antra su dviem variantais

--- task1/main.js
+++ task1/main.js
@@ -2,3 +2,87 @@
   Parašyti JS kodą, kuris leis vartotojui įvesti temperatūrą celsijumi arba farenheitu.
   Įvedus celsijų - parodoma kiek temperatūros tai būtų farenheitu | įvedus farenheitą - parodoma kiek temperatūros tai būtų celsijumi.
 */
+
+function convertTemperature() {
+
+  // Nuskaitome įvestą temperatūrą ir parenkame vienetus
+  const temperature = document.getElementById('temperature').value;
+  const units = document.querySelector('input[name="units"]:checked').value;
+
+  // Konvertuojame temperatūrą ir išvedame rezultatą
+  if (units === 'celsius') {
+    const fahrenheit = temperature * 1.8 + 32;
+    document.getElementById('result').innerHTML = `${temperature}&deg;C yra ${fahrenheit}&deg;F`;
+  } else {
+    const celsius = (temperature - 32) / 1.8;
+    document.getElementById('result').innerHTML = `${temperature}&deg;F yra ${celsius}&deg;C`;
+  }
+}
+
+
+
+/*const form = document.createElement('form');
+
+const label = document.createElement('label');
+label.setAttribute('for', 'temperature');
+label.textContent = 'Įveskite temperatūrą:';
+form.appendChild(label);
+
+const input = document.createElement('input');
+input.setAttribute('type', 'text');
+input.setAttribute('id', 'temperature');
+input.setAttribute('name', 'temperature');
+form.appendChild(input);
+
+const celsiusRadio = document.createElement('input');
+celsiusRadio.setAttribute('type', 'radio');
+celsiusRadio.setAttribute('id', 'celsius');
+celsiusRadio.setAttribute('name', 'units');
+celsiusRadio.setAttribute('value', 'celsius');
+form.appendChild(celsiusRadio);
+
+const celsiusLabel = document.createElement('label');
+celsiusLabel.setAttribute('for', 'celsius');
+celsiusLabel.textContent = 'Celsijaus';
+form.appendChild(celsiusLabel);
+
+const fahrenheitRadio = document.createElement('input');
+fahrenheitRadio.setAttribute('type', 'radio');
+fahrenheitRadio.setAttribute('id', 'fahrenheit');
+fahrenheitRadio.setAttribute('name', 'units');
+fahrenheitRadio.setAttribute('value', 'fahrenheit');
+form.appendChild(fahrenheitRadio);
+
+const fahrenheitLabel = document.createElement('label');
+fahrenheitLabel.setAttribute('for', 'fahrenheit');
+fahrenheitLabel.textContent = 'Farenheito';
+form.appendChild(fahrenheitLabel);
+
+const button = document.createElement('button');
+button.setAttribute('type', 'button');
+button.addEventListener('click', convertTemperature);
+button.textContent = 'Konvertuoti';
+form.appendChild(button);
+
+document.body.appendChild(form);
+
+const result = document.createElement('p');
+result.setAttribute('id', 'result');
+document.body.appendChild(result);
+
+function convertTemperature() {
+  // Nuskaitome įvestą temperatūrą ir parenkame vienetus
+  const temperature = document.getElementById('temperature').value;
+  const units = document.querySelector('input[name="units"]:checked').value;
+
+  // Konvertuojame temperatūrą ir išvedame rezultatą
+  if (units === 'celsius') {
+    const fahrenheit = temperature * 1.8 + 32;
+    document.getElementById('result').innerHTML = `${temperature}&deg;C yra ${fahrenheit}&deg;F`;
+  } else {
+    const celsius = (temperature - 32) / 1.8;
+    document.getElementById('result').innerHTML = `${temperature}&deg;F yra ${celsius}&deg;C`;
+  }
+}*/
+
+
--- task2/main.js
+++ task2/main.js
@@ -1,3 +1,38 @@
 /*
   Parašyti JS kodą, kuris skaičiuos kiek kartų buvo paspausta ant vieno arba kito elemento ekrane. (Ekrane turėti 2 nuotraukas ir laukelį po jomis, kuriame bus atvaizduojama kiek kartų buvo paspausta ant kurios nuotraukos)
 */
+function countClicks(element) {
+  // element yra nuotrauka, ant kurios buvo paspausta
+  // Paimkite elemento id
+  let elementId = element.id;
+  // Paimkite span elementą, kuris atvaizduoja kiek kartų buvo paspausta ant nuotraukos
+  let clickElement = document.getElementById(elementId + "Clicks");
+  // Padidinkite kiek kartų buvo paspausta ant nuotraukos skaičių vienetu
+  let currentClicks = parseInt(clickElement.innerHTML);
+  clickElement.innerHTML = currentClicks + 1;
+}
+
+
+/*function countClicks(element) {
+  // element yra nuotrauka, ant kurios buvo paspausta
+  // Paimkite elemento id
+  let elementId = element.id;
+  // Paimkite span elementą, kuris atvaizduoja kiek kartų buvo paspausta ant nuotraukos
+  let spanElement = document.getElementById(elementId + "Clicks");
+  // Padidinkite kiek kartų buvo paspausta ant nuotraukos skaičių vienetu
+  spanElement.innerHTML = (parseInt(spanElement.innerHTML) + 1).toString();
+}
+*/
+
+let counter1 = 0;
+      let counter2 = 0;
+
+      function incrementCounter(imageId) {
+        if (imageId === 'image1') {
+          counter1++;
+          document.getElementById('counter1').innerHTML = `Paspausta ${counter1} kartų`;
+        } else if (imageId === 'image2') {
+          counter2++;
+          document.getElementById('counter2').innerHTML = `Paspausta ${counter2} kartų`;
+        }
+      }
